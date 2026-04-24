2a47c17fcfa6782a881a64578859349916582379
Waffle0823 <csshin9928@gmail.com>
2026-04-25 01:45:05 +0900

    Consolidate SDK download logic into helper function and move helper include to top of CMakeLists

--- cmake/SteamworksSDKHelpers.cmake
+++ cmake/SteamworksSDKHelpers.cmake
@@ -1,3 +1,42 @@
+function(_steamworks_download_sdk OUT_SDK_ROOT)
+    if(NOT DEFINED ENV{STEAM_LOGIN_SECURE})
+        message(FATAL_ERROR "STEAM_LOGIN_SECURE env var must be set")
+    endif()
+
+    set(_swsdk_zip "${CMAKE_BINARY_DIR}/external/steamworks_sdk_${SWSDK_VERSION}.zip")
+
+    if(NOT EXISTS ${_swsdk_zip})
+        message(STATUS "Downloading SteamWorks SDK version ${SWSDK_VERSION} from ${SWSDK_LINK}")
+
+        set(_swsdk_zip_part "${_swsdk_zip}.part")
+
+        file(
+            DOWNLOAD
+            ${SWSDK_LINK}
+            ${_swsdk_zip_part}
+            STATUS status
+            SHOW_PROGRESS
+            HTTPHEADER "Cookie: steamLoginSecure=$ENV{STEAM_LOGIN_SECURE}"
+        )
+
+        list(GET status 0 status_code)
+        if(NOT status_code EQUAL 0)
+            file(REMOVE ${_swsdk_zip_part})
+            message(FATAL_ERROR "Download failed: ${status}")
+        endif()
+
+        file(RENAME ${_swsdk_zip_part} ${_swsdk_zip})
+    endif()
+
+    if(NOT EXISTS ${CMAKE_BINARY_DIR}/external/steamworks_sdk_${SWSDK_VERSION})
+        message(STATUS "Extracting SteamWorks SDK")
+
+        file(ARCHIVE_EXTRACT INPUT ${CMAKE_BINARY_DIR}/external/steamworks_sdk_${SWSDK_VERSION}.zip DESTINATION ${CMAKE_BINARY_DIR}/external/steamworks_sdk_${SWSDK_VERSION})
+    endif()
+
+    set(${OUT_SDK_ROOT} "${CMAKE_BINARY_DIR}/external/steamworks_sdk_${SWSDK_VERSION}/sdk" PARENT_SCOPE)
+endfunction()
+
 function(_steamworks_auto_copy_runtime)
     set(_files ${ARGN})
     if(NOT _files)
